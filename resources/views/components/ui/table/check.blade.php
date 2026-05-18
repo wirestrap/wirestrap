91fd0f805d12b12e3fe48edc1a746c48fdfcbaf5
Damien FOSSEY <fosseyd.dev@proton.me>
2026-05-18 21:41:54 +0200

    fix table bulk: skip disabled/readonly checkboxes

--- resources/views/components/ui/table/check.blade.php
+++ resources/views/components/ui/table/check.blade.php
@@ -1,11 +1,13 @@
 @props([
     'classTd' => '',
+    'disabled' => false,
 ])
 
 <td @if ($classTd) class="{{ $classTd }}" @endif>
     <input
         type="checkbox"
         data-ws-bulk x-bind="wsBulkCheck"
+        @disabled($disabled)
         {{ $attributes->merge(['class' => 'ws-form-check-input']) }}
     />
 
